d03bae4cb5b428e6f22c96b4c7b007f63d493869
hehe1111 <2908749709@qq.com>
2025-07-12 13:24:04 +0800

    test: 准备一个测试用的 .vue 文件

---
+++ fixtures/vue-sfc-test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <slot>
+      <span>这是一段中文</span>
+    </slot>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less">
+.card {
+  width: 200px;
+  height: 100px;
+  background: #eee;
+  border: 1px solid #ddd;
+  display: flex;
+  padding: 10;
+  justify-content: center;
+  align-items: center;
+}
+</style>
