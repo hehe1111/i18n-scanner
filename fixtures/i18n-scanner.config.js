93afc0e114e4315e05f6ff9fd2276095e5fe17c7
hehe1111 <2908749709@qq.com>
2025-07-12 17:36:43 +0800

    test(fixtures): add exclude pattern example to i18n-scanner.config file to demonstrate filtering capabilities

--- fixtures/i18n-scanner.config.js
+++ fixtures/i18n-scanner.config.js
@@ -2,5 +2,6 @@ export default {
   i18nCallee: 'i18nFake.t',
   output: './here-is-where-i-put-my-i18n-files',
   importStatement:
-    'import i18nFake from "my-awesome-i18n-lib-that-does-not-exist"'
+    'import i18nFake from "my-awesome-i18n-lib-that-does-not-exist"',
+  exclude: ['**/*.config.js']
 }
